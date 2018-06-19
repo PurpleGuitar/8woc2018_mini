359945c11f4960c50f6db96cdedca10697c0d88a
Craig Oliver <craig.robert.oliver@gmail.com>
2018-06-19 16:31:59 -0400

    Convert USFM to Markdown

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -22,6 +22,8 @@ fun main(args: Array<String>) {
             .flatMap { it.books() }
             .filter { it.slug == bookCode }
             .flatMap { httpGet(it.src) }
+            .flatMap { it.body().use { Observable.from(it.string().lines()) } }
+            .flatMap { usfmToMarkdown(it) }
             .subscribe { println(it) }
 }
 
@@ -29,4 +31,20 @@ fun httpGet(url: String): Observable<Response> {
     return Observable.fromCallable() {
         OkHttpClient.Builder().build().newCall(Request.Builder().url(url).build()).execute()
     }
+}
+
+val HEADER = Regex("^\\\\h (.*)$")
+val CHAPTER = Regex("^\\\\c (.*)$")
+val VERSE = Regex("^\\\\v (\\d+) (.*)$")
+fun usfmToMarkdown(line: String): Observable<String> {
+    HEADER.matchEntire(line)?.let {
+        return Observable.just("# " + it.groupValues[1])
+    }
+    CHAPTER.matchEntire(line)?.let {
+        return Observable.just("\n## Chapter " + it.groupValues[1] + "\n")
+    }
+    VERSE.matchEntire(line)?.let {
+       return Observable.just("(" + it.groupValues[1] +") " + it.groupValues[2])
+    }
+    return Observable.empty()
 }
