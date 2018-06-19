14459300f9994f48485d7452ebb260c76e0eeccd
Craig Oliver <craig.robert.oliver@gmail.com>
2018-06-19 10:45:43 -0400

    Pulled catalog

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,29 @@
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.rxkotlin.toObservable
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import rx.Observable
+
+data class Catalog ( val mod: String = "")
+
+interface UnfoldingWordAPI {
+
+    @GET("/uw/txt/2/catalog.json")
+    fun catalog(): Observable<Catalog>
+
+}
 
 fun main(args: Array<String>) {
 
-    val list = listOf("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
-
-    list.toObservable() // extension function for Iterables
-            .filter { it.length >= 5 }
-            .subscribeBy(  // named arguments for lambda Subscribers
-                    onNext = { println(it) },
-                    onError =  { it.printStackTrace() },
-                    onComplete = { println("Done!") }
-            )
+    val builder = Retrofit.Builder()
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("https://api.unfoldingword.org")
+    val client = OkHttpClient.Builder().build()
+    builder.client(client)
+    val api = builder.build().create(UnfoldingWordAPI::class.java)
+    val catalog = api.catalog()
+    catalog.subscribe { println(it)}
 
 }
