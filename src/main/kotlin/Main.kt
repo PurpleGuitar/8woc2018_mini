9e54f847b6f0d740565acbcd73f666a8cffa3eab
Craig Oliver <craig.robert.oliver@gmail.com>
2018-06-19 10:53:10 -0400

    whitespace

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,6 @@ fun main(args: Array<String>) {
 
     val api = createUnfoldingWordService();
     val catalog = api.catalog()
-    catalog.subscribe { println(it)}
+    catalog.subscribe { println(it) }
 
 }
