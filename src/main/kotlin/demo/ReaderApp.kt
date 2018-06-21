729c9b756b0d7b14bcf1cc302b86c599da15baa5
Craig Oliver <craig.robert.oliver@gmail.com>
2018-06-21 14:21:35 -0400

    Running on right threads

--- src/main/kotlin/demo/ReaderApp.kt
+++ src/main/kotlin/demo/ReaderApp.kt
@@ -2,15 +2,11 @@ package demo
 
 import com.github.thomasnield.rxkotlinfx.actionEvents
 import com.github.thomasnield.rxkotlinfx.observeOnFx
-import com.sun.javafx.fxml.builder.JavaFXSceneBuilder
 import getUWContentURL
 import httpGet
 import io.reactivex.Observable
-import io.reactivex.functions.BiConsumer
-import io.reactivex.rxjavafx.schedulers.JavaFxScheduler
 import io.reactivex.schedulers.Schedulers
 import javafx.scene.control.TextArea
-import javafx.scene.control.TextField
 import tornadofx.*
 import usfmToMarkdown
 
@@ -19,29 +15,35 @@ class ReaderApp : App(ReaderView::class)
 class ReaderView : View() {
     var textArea: TextArea by singleAssign()
     override val root = vbox {
-        button("Load it!")
-                .actionEvents()
-                .doOnNext { println("Emitting " + it + " on thread " + Thread.currentThread().name)}
-                .subscribe {
-                    textArea.text = "Loading, please wait..."
-                    getUWContentURL(
-                            "bible",
-                            "en",
-                            "ulb-en",
-                            "gen")
-                            .observeOn(Schedulers.io())
-                            .doOnNext { println("Loading USFM on thread " + Thread.currentThread().name)}
-                            .flatMap { httpGet(it) }
-                            .flatMap { it.body().use { Observable.fromIterable(it.string().lines()) } }
-                            .flatMap { usfmToMarkdown(it) }
-                            .collectInto(StringBuilder(), { builder, item -> builder.append(item + "\n") })
-                            .toObservable()
-                            .observeOnFx()
-                            .doOnNext { println("Updating UI on thread " + Thread.currentThread().name)}
-                            .subscribe { sb ->
-                                textArea.text = sb.toString()
-                            }
-                }
+        button("Read it!") {
+            actionEvents().subscribe {
+                textArea.text = "Loading catalog, please wait..."
+            }
+            actionEvents()
+                    .observeOn(Schedulers.io())
+                    .subscribe {
+                        getUWContentURL(
+                                "bible",
+                                "en",
+                                "ulb-en",
+                                "gen")
+                                .observeOnFx()
+                                .doOnNext { textArea.text = "Loading USFM, please wait..." }
+                                .observeOn(Schedulers.io())
+                                .flatMap { httpGet(it) }
+                                .observeOnFx()
+                                .doOnNext { textArea.text = "Converting to Markdown, please wait..." }
+                                .observeOn(Schedulers.io())
+                                .flatMap { it.body().use { Observable.fromIterable(it.string().lines()) } }
+                                .flatMap { usfmToMarkdown(it) }
+                                .collectInto(StringBuilder(), { builder, item -> builder.append(item + "\n") })
+                                .toObservable()
+                                .observeOnFx()
+                                .subscribe { sb ->
+                                    textArea.text = sb.toString()
+                                }
+                    }
+        }
         textarea {
             textArea = this
             isEditable = false
